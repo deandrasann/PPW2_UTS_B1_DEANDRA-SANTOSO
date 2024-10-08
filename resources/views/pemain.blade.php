9a6b6ef122f3b8382f3246fffadf54b964224e82
deandrasann <deandrasantosoo@gmail.com>
2024-10-08 14:31:30 +0700

    first commit

--- resources/views/pemain.blade.php
+++ resources/views/pemain.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Pemain</title>
-</head>
-<body>
+@extends('headerfooter.footer')
+@section('content')
     <table>
         <thead>
             <tr>
@@ -27,5 +20,4 @@
             @endforeach
         </tbody>
     </table>
-</body>
-</html>
+@endsection
